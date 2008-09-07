9f45c96428435a2f54614f8e80e996b58b30b226
Larry Olson <loarabia@hotmail.com>
2008-09-06 18:57:13 -0700

    Added the Mp3MediaStreamSourceCode along with the Tests cases for it. This is just the initial checkin.

---
+++ Mp3MediaStreamSource/Mp3MediaStreamSource.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Mp3MediaStreamSource
+{
+    public class Mp3MediaStreamSource: MediaStreamSource
+    {
+
+        protected override void CloseMedia()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void GetDiagnosticAsync(MediaStreamSourceDiagnosticKind diagnosticKind)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void GetSampleAsync(MediaStreamType mediaStreamType)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OpenMediaAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SeekAsync(long seekToTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SwitchMediaStreamAsync(MediaStreamDescription mediaStreamDescription)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
